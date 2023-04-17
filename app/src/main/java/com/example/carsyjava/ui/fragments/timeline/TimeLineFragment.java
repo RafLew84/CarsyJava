0e6c3a203601132c3ebad1f1a82c590593674e9e
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-04-17 16:06:17 +0200

    TimeLineFragment

--- app/src/main/java/com/example/carsyjava/ui/fragments/timeline/TimeLineFragment.java
+++ app/src/main/java/com/example/carsyjava/ui/fragments/timeline/TimeLineFragment.java
@@ -10,10 +10,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
+import com.example.carsyjava.R;
+import com.example.carsyjava.data.Car;
 import com.example.carsyjava.data.DataProvider;
 import com.example.carsyjava.databinding.FragmentTimeLineBinding;
 import com.example.carsyjava.ui.fragments.timeline.adapters.TimeLineAdapter;
+
+import java.util.stream.Collectors;
 
 public class TimeLineFragment extends Fragment {
 
@@ -23,14 +29,74 @@ public class TimeLineFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentTimeLineBinding.inflate(inflater);
-        setupRecyclerView();
+
         return binding.getRoot();
     }
 
-    private void setupRecyclerView() {
+    @Override
+    public void onResume() {
+        super.onResume();
+        final RecyclerView recyclerView = setupRecyclerView();
+        setupDropDownSelector(recyclerView);
+    }
+
+    private RecyclerView setupRecyclerView() {
         RecyclerView recyclerView = binding.timeLineRecyclerView;
         recyclerView.setAdapter(new TimeLineAdapter(DataProvider.cars.get(0).getCosts(), requireContext()));
         recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        return recyclerView;
     }
 
+    private void setupDropDownSelector(RecyclerView recycler) {
+        AutoCompleteTextView textView = binding.autoCompleteTextView;
+        textView.setDropDownBackgroundResource(R.color.dark_blue_900);
+
+        if (textView.getText().toString().isEmpty())
+            textView.setText(DataProvider.cars.get(0).getName());
+
+        ArrayAdapter<String> adapter = getStringArrayAdapter();
+        textView.setAdapter(adapter);
+
+        setAdapterAtPosition(recycler);
+
+        textView.setOnItemClickListener((parent, view, position, id) ->
+                getAdapter(recycler, position));
+    }
+
+    private void setAdapterAtPosition(RecyclerView recycler) {
+        int itemPosition = -1;
+        for (int i = 0; i < DataProvider.cars.size(); i++) {
+            if (DataProvider
+                    .cars
+                    .get(i)
+                    .getName()
+                    .equals(binding.autoCompleteTextView.getText().toString())) {
+                itemPosition = i;
+                break;
+            }
+        }
+
+        if (itemPosition != -1) {
+            getAdapter(recycler, itemPosition);
+        }
+    }
+
+    private void getAdapter(RecyclerView recycler, int position) {
+        recycler.swapAdapter(
+                new TimeLineAdapter(
+                        DataProvider.cars.get(position).getCosts(),
+                        requireContext()
+                ),
+                true);
+    }
+
+    @NonNull
+    private ArrayAdapter<String> getStringArrayAdapter() {
+        return new ArrayAdapter<>(
+                requireContext(),
+                R.layout.dropdown_item,
+                DataProvider.cars.stream()
+                        .map(Car::getName)
+                        .collect(Collectors.toList()));
+    }
 }
